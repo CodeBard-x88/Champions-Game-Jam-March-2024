76e0b91e09808b71dbaf3a42684532d44ef632b2
CodeBard_x88 <solo.developer29@gmail.com>
2024-03-09 21:11:21 +0500

    sailing ship

--- Assets/Scripts/ShipMovment.cs
+++ Assets/Scripts/ShipMovment.cs
@@ -8,18 +8,26 @@ public class ShipMovment : MonoBehaviour
 
     [SerializeField]
     private GameObject shipTarget;
+
+    private bool hasnotReached;
     void Start()
     {
         nm = GetComponent<NavMeshAgent>();
+        hasnotReached = true;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(shipTarget != null)
+        if(this.transform.position == shipTarget.transform.position)
         {
-            Vector3 targetPos = new Vector3(shipTarget.transform.position.x, 0, shipTarget.transform.position.z);
+            hasnotReached = false;
+        }
+
+        if(nm.isActiveAndEnabled && nm.isOnNavMesh && shipTarget != null && hasnotReached)
+        {
+            Vector3 targetPos = new Vector3(shipTarget.transform.position.x, this.transform.position.y, shipTarget.transform.position.z);
             UnityEngine.Debug.Log(targetPos);
             UnityEngine.Debug.Log("Setting Navemsh Agent Destination");
             UnityEngine.Debug.Log("NavMeshAgent Location: "+this.transform.position);
@@ -28,7 +36,15 @@ public class ShipMovment : MonoBehaviour
         }
         else
         {
-            UnityEngine.Debug.Log("Ship Target is null.");
+            if (!nm.isActiveAndEnabled)
+                UnityEngine.Debug.LogWarning("NavMeshAgent is not active.");
+            if (!nm.isOnNavMesh)
+                UnityEngine.Debug.LogWarning("NavMeshAgent is not placed on a NavMesh.");
+            if (shipTarget == null)
+                UnityEngine.Debug.LogWarning("Ship Target is null.");
+
+            if (!hasnotReached)
+                UnityEngine.Debug.Log("Ship has reached the island.");
         }
     }
 }
